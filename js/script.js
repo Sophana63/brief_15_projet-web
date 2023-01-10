f41769f4cb74e1d973786937df12c12d939aadbc
zeugy <rpdev@protonmail.com>
2023-01-10 16:53:23 +0100

    api avec des petits vélos

--- js/script.js
+++ js/script.js
@@ -15,21 +15,28 @@ async function fetchContract(city) {
 
 function displayStations(city) {
 
-    var map = L.map('map')
+    var velo_icon = L.icon({
+        iconUrl: './images/velo-png-2.png',
+        iconSize: [32, 32], // size of the icon
+        iconAnchor: [16, 16], // point of the icon which will correspond to marker's location
+        popupAnchor: [16, -50] // point from which the popup should open relative to the iconAnchor
+    });
 
+    var map = L.map('map');
     fetchContract(city).then(function (datas) {
         map.setView([datas[0].position.lat, datas[0].position.lng], 13);
         for (var i = 0; i < datas.length; i++) {
-            var marker = L.marker([datas[i].position.lat, datas[i].position.lng]).addTo(map);
+            var marker = L.marker([datas[i].position.lat, datas[i].position.lng], { icon: velo_icon }).addTo(map);
         }
 
     })
-
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>' //obligatoire
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>', //obligatoire
     }).addTo(map);
 }
+
+
 
 let city = "Lyon";
 displayStations(city);
