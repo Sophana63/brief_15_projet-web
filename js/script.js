8a32d0860cd7c71024a5b7eda512f1af4294ec14
zeugy <rpdev@protonmail.com>
2023-01-10 15:27:11 +0100

    api fonctionnelle

--- js/script.js
+++ js/script.js
@@ -15,18 +15,20 @@ async function fetchContract(city) {
 
 function displayStations(city) {
 
-    var map = L.map('map').setView([45.75, 4.85], 12);
-    L.tileLayer('https://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>' //obligatoire
-    }).addTo(map);
+    var map = L.map('map')
 
     fetchContract(city).then(function (datas) {
+        map.setView([datas[0].position.lat, datas[0].position.lng], 13);
         for (var i = 0; i < datas.length; i++) {
             var marker = L.marker([datas[i].position.lat, datas[i].position.lng]).addTo(map);
         }
 
     })
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>' //obligatoire
+    }).addTo(map);
 }
 
 let city = "Lyon";
